ac04037fdbc3d86b516098832a678f9ab9833f4b
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-01 12:33:05 -0500

    Create #show action to store a given id's article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
-#This should be a comment about this controller
+#This is the Controller for the Articles
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
